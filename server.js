ae14dfad258da29d22f0adac42d77c48e65566b1
yeahimamaan <m.amaanbhatti@gmail.com>
2025-12-14 13:20:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,26 +2,21 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
-import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Fix for __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 app.use(cors());
 app.use(express.json());
 
-// Serve static files
-app.use(express.static(__dirname));
+// Serve all files in current folder (so index.html, scripts, etc. are found)
+app.use(express.static(path.resolve(".")));
 
 // Home route
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.resolve("index.html")); // <- This makes sure Render finds index.html
 });
 
 app.listen(PORT, () => {
